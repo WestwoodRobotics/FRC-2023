786ce5b93a3f210f0ff56f564e37407179b1a7bc
Niktopia <87048477+Niktopia@users.noreply.github.com>
2022-11-17 19:20:23 -0600

    Added Primitive Comments

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -4,7 +4,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation2d;  //Imports
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.AnalogGyro;
 /** Represents a swerve drive style drivetrain. */
 public class SwerveDrive {
   public static final double kMaxSpeed = 3.0; // 3 meters per second
-  public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
+  public static final double kMaxAngularSpeed = Math.PI; // 1/2 of a revolution per second
 
   private final Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
   private final Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
