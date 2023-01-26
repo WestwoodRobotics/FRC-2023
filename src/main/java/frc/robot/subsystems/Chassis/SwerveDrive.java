be20edfe510de170e9c0ebb4dbe5ebd03c603c12
Niktopia <87048477+Niktopia@users.noreply.github.com>
2023-01-26 00:39:17 +0000

    Updated SwerveDrive.java to use Motor Ports from Constants.java

--- src/main/java/frc/robot/subsystems/Chassis/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/Chassis/SwerveDrive.java
@@ -2,12 +2,13 @@ package frc.robot.subsystems.Chassis;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Chassis.SwerveModule;
+import frc.robot.Constants.*;
 
 // This class represents the swerve drive system, which is composed of 4 swerve modules (one for each wheel)
 public class SwerveDrive extends SubsystemBase {
     // Initialize the 4 swerve modules with the appropriate CAN IDs, inversion states, and encoder offsets
-    SwerveModule frontLeftModule = new SwerveModule(0, 1, false, false, 0, 0, false);
-    SwerveModule frontRightModule = new SwerveModule(2, 3, false, false, 0, 0, false);
-    SwerveModule backLeftModule = new SwerveModule(4, 5, false, false, 0, 0, false);
-    SwerveModule backRightModule = new SwerveModule(6, 7, false, false, 0, 0, false);
+    SwerveModule frontLeftModule = new SwerveModule(ModuleConstants.kFrontLeftDriveMotorPort, ModuleConstants.kFrontLeftSteerMotorPort, false, false, 0, 0, false);
+    SwerveModule frontRightModule = new SwerveModule(ModuleConstants.kFrontRightDriveMotorPort, ModuleConstants.kFrontRightSteerMotorPort, false, false, 0, 0, false);
+    SwerveModule backLeftModule = new SwerveModule(ModuleConstants.kBackLeftDriveMotorPort, ModuleConstants.kBackLeftSteerMotorPort, false, false, 0, 0, false);
+    SwerveModule backRightModule = new SwerveModule(ModuleConstants.kBackRightDriveMotorPort, ModuleConstants.kBackRightSteerMotorPort, false, false, 0, 0, false);
 }
