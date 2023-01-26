246e6d81f4f94a309a51276e50f9b0ec0ff5f8b3
SwordCraft-Hero <arunmsn19@gmail.com>
2023-01-26 11:33:29 -0600

    SpacingFix

--- src/main/java/frc/robot/subsystems/Intake/IntakeModule.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeModule.java
@@ -1,9 +1,7 @@
 package frc.robot.subsystems.Intake;
 
-
 import frc.robot.subsystems.Intake.IntakeModule;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 
 public class IntakeModule {
     // Motor for the intake mechanism
@@ -12,26 +10,26 @@ public class IntakeModule {
     private double intakeEncoderPosition;
     // Encoder for measuring the velocity of the intake motor
     private double intakeEncoderVelocity;
+
     // Constructor for initializing the intake module
-    public IntakeModule(int intakeMotorId, int intakeEncoderId){
+    public IntakeModule(int intakeMotorId, int intakeEncoderId) {
         intakeMotor = new WPI_TalonSRX(intakeMotorId);
         intakeEncoderPosition = intakeMotor.getSelectedSensorPosition();
         intakeEncoderVelocity = intakeMotor.getSelectedSensorVelocity();
     }
-    
-    //Retruns the position of the intake encoder
-    public double intakeEncoderPosition(){
+
+    // Retruns the position of the intake encoder
+    public double intakeEncoderPosition() {
         return intakeEncoderPosition;
     }
-    //Returns the velocity of the intake encoder
-    public double intakeEncoderVelocity(){
+
+    // Returns the velocity of the intake encoder
+    public double intakeEncoderVelocity() {
         return intakeEncoderVelocity;
     }
-    //Sets the speed of the intake motor
-    public void setIntakeMotor(double speed){
+
+    // Sets the speed of the intake motor
+    public void setIntakeMotor(double speed) {
         intakeMotor.set(speed);
     }
-
-
-
 }
