6b65e309f362da891bc35af7488aa2a4abde10ad
Niktopia <87048477+Niktopia@users.noreply.github.com>
2023-01-26 00:38:21 +0000

    Added getter methods for SwerveModule.java

--- src/main/java/frc/robot/subsystems/Chassis/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/Chassis/SwerveModule.java
@@ -21,47 +21,83 @@ public class SwerveModule {
     private final boolean absoluteEncoderReversed;// Boolean for determining if the absolute encoder is reversed
     private final double absoluteEncoderOffsetRad;// Offset for the absolute encoder
 
-    // The constructor takes in several parameters for configuring the motors, encoders, and PID controllers
-    
+    // The constructor
     public SwerveModule(int driveMotorId, int steerMotorId, boolean driveMotorReversed, boolean steerMotorReversed, int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed){
         this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
         this.absoluteEncoderReversed = absoluteEncoderReversed;
         absoluteEncoder = new AnalogInput(absoluteEncoderId);
 
         // Initialize the drive and steer motors using the provided CAN IDs
-        
         driveMotor = new WPI_TalonSRX(driveMotorId);
         steerMotor = new WPI_TalonSRX(steerMotorId);
 
         // Set the inversion for the drive and steer motors based on the provided booleans
-        
         driveMotor.setInverted(driveMotorReversed);
         steerMotor.setInverted(steerMotorReversed);
 
-         // Get the initial position and velocity of the encoders
-        
+        // Get the initial position and velocity of the encoders
         driveEncoderPosition = driveMotor.getSelectedSensorPosition();
         driveEncoderVelocity = driveMotor.getSelectedSensorVelocity();
         steerEncoderPosition = steerMotor.getSelectedSensorPosition();
         steerEncoderVelocity = steerMotor.getSelectedSensorVelocity();
 
-         // Set the encoder coefficients for the drive and steer motors
-
+        // Set the encoder coefficients for the drive and steer motors
         driveMotor.configSelectedFeedbackCoefficient(ModuleConstants.kDriveEncoderRot2Meter);
         driveMotor.configSelectedFeedbackCoefficient(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
         steerMotor.configSelectedFeedbackCoefficient(ModuleConstants.kSteerEncoderRot2Rad);
         steerMotor.configSelectedFeedbackCoefficient(ModuleConstants.kSteerEncoderRPM2RadPerSec);
         
         // Initialize the drive and steer PID controllers using the constants from DriveConstants
-
         drivePIDController = new PIDController(DriveConstants.kPSwerveDriveDriveMotor, 0, 0);
         steerPIDController = new PIDController(DriveConstants.kPSwerveDriveSteerMotor, 0, 0);
         steerPIDController.enableContinuousInput(-Math.PI, Math.PI);
 
         // Reset the encoder positions to zero
-
         driveMotor.setSelectedSensorPosition(0); // Resets the  Drive Motor Encoder
         steerMotor.setSelectedSensorPosition(0); // Resets the Steer Motor Encoder
     }
+
     
+    // Method for getting the drive encoder position
+    public double getDriveEncoderPosition(){
+        return driveEncoderPosition;
+    }
+
+    // Method for getting the drive encoder velocity
+    public double getDriveEncoderVelocity(){
+        return driveEncoderVelocity;
+    }
+
+    // Method for getting the steer encoder position
+    public double getSteerEncoderPosition(){
+        return steerEncoderPosition;
+    }
+
+    // Method for getting the steer encoder velocity
+    public double getSteerEncoderVelocity(){
+        return steerEncoderVelocity;
+    }
+
+    // Method for getting the absolute encoder position
+    public double getAbsoluteEncoderPosition(){
+        return absoluteEncoder.getAverageVoltage();
+    }
+
+
+    public void setDriveMotorSpeed(double speed){
+        driveMotor.set(speed);
+    }
+
+    public void setSteerMotorSpeed(double speed){
+        steerMotor.set(speed);
+    }
+
+
+
+
+
+
+    
+
+
 }
