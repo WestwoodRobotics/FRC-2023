fc7b1389b17082f56158193f1ad30c2a0cd358f7
Nikhil <nikhilb186199@gmail.com>
2023-02-17 21:25:34 -0600

    Commented Out SwerveDriveOld (For Reference)
    Removed AutonPath stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,7 +20,7 @@ import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.PortConstants;
 import frc.robot.commands.SwerveDriveCommands.DriveConstantControlCommand;
-import frc.robot.subsystems.swerve.SwerveDriveOld;
+import frc.robot.subsystems.swerve.SwerveDrive;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -37,7 +37,7 @@ public class RobotContainer {
   private final XboxController primaryController = new XboxController(PortConstants.XboxController1);
   private final XboxController secondaryController = new XboxController(PortConstants.XboxController2);
   // The robot's subsystems and commands are defined here...
-  private final SwerveDriveOld SwerveDriveSystem = new SwerveDriveOld();
+  private final SwerveDrive SwerveDriveSystem = new SwerveDrive();
 
   //Instantiating the timer
   private final Timer timer = new Timer();
@@ -95,46 +95,6 @@ public class RobotContainer {
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    */
-  public Command getAutonomousCommand() {
-
-    // This will load the file "Example Path Group.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
-// for every path in the group
-    ArrayList<PathPlannerTrajectory> pathGroup1 = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup("New Path", new PathConstraints(4, 3));
-
-// This will load the file "Example Path Group.path" and generate it with different path constraints for each segment
-    ArrayList<PathPlannerTrajectory> pathGroup2 = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup(
-      "New Path",
-      new PathConstraints(4, 3),
-      new PathConstraints(2, 2),
-      new PathConstraints(3, 3));
-    // This will load the file "FullAuto.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
-// for every path in the group
-    ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup("FullAuto", new PathConstraints(4, 3));
-
-// This is just an example event map. It would be better to have a constant, global event map
-// in your code that will be used by all path following commands.
-    HashMap<String, Command> eventMap = new HashMap<>();
-    eventMap.put("marker1", new PrintCommand("Passed marker 1"));
-//eventMap.put("intakeDown", new IntakeDown())
-
-
-// Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
-    SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
-      SwerveDriveSystem::getPoseMeters, // Pose2d supplier
-      SwerveDriveSystem::resetPose, // Pose2d consumer, used to reset odometry at the beginning of auto
-      Constants.DriveConstants.kDriveKinematics, // SwerveDriveKinematics
-      new PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
-      new PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
-      SwerveDriveSystem::setStates, // Module states consumer used to output to the drive subsystem
-      eventMap,
-      true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-      SwerveDriveSystem // The drive subsystem. Used to properly set the requirements of path following commands
-
-    );
-
-    return autoBuilder.fullAuto(pathGroup);
-
-  }
 
   public void teleopTimer() {
     timer.reset();
