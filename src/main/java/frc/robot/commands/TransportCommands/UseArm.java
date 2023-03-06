8a0eb35d460be44f0dfedac0c957aaea859bf72d
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2023-03-06 00:03:23 +0000

    Added Macros For Transport Subsystem (Needs significant testing)

--- src/main/java/frc/robot/commands/TransportCommands/UseArm.java
+++ src/main/java/frc/robot/commands/TransportCommands/UseArm.java
@@ -20,22 +20,49 @@ public class UseArm extends CommandBase {
     public void execute() {
         // This probably shouldn't be in a constant control command
         // Also what do these do?
-        if(controller.getAButton()) {
-            m_transport.setShoulderMotorPosition(20000);
-        } 
-        else if(controller.getBButton()) {
-            m_transport.setElbowMotorPosition(20000);
-        } 
-        // This is fine in constant control command
-        else {
-            m_transport.setShoulderMotorPower(
-                Conversions.deadZoneSquare(-1 * controller.getLeftY(), 0.1));
-            
-            m_transport.setElbowMotorPower(
-                Conversions.deadZoneSquare(controller.getRightY(), 0.1));
-                           
-            m_transport.setWristMotorPower(
-                Conversions.deadZoneSquare(controller.getRightX(), 0.1));
+
+        if (controller.getAButton()){
+            while (m_transport.getWristMotorEncoderTicks() < 2048){
+                m_transport.setWristMotorPower(0.5);
+            }
+            m_transport.setShoulderMotorPower(0);
         }
+        else if (controller.getBButton()){
+            while (m_transport.getWristMotorEncoderTicks() != 0){
+                m_transport.setWristMotorPower(-0.5);
+            }
+            m_transport.setWristMotorPower(0);
+        }
+
+        if (controller.getXButton()){
+            while (m_transport.getShoulderMotorLeadEncoderTicks() < 1024){ //TODO: Update to actual scoring ticks
+                m_transport.setShoulderMotorPower(0.5);
+            }
+            m_transport.setElbowMotorPower(0);
+        }
+        else if (controller.getYButton()){
+            while (m_transport.getElbowMotorEncoderTicks() != 0){
+                m_transport.setElbowMotorPower(-0.5);
+            }
+            m_transport.setElbowMotorPower(0);
+        }
+
+        // if(controller.getAButton()) {
+        //     m_transport.setShoulderMotorPosition(20000);
+        // } 
+        // else if(controller.getBButton()) {
+        //     m_transport.setElbowMotorPosition(20000);
+        // } 
+        // // This is fine in constant control command
+        // else {
+        //     m_transport.setShoulderMotorPower(
+        //         Conversions.deadZoneSquare(-1 * controller.getLeftY(), 0.1));
+            
+        //     m_transport.setElbowMotorPower(
+        //         Conversions.deadZoneSquare(controller.getRightY(), 0.1));
+                           
+        //     m_transport.setWristMotorPower(
+        //         Conversions.deadZoneSquare(controller.getRightX(), 0.1));
+        // }
     }
 }
