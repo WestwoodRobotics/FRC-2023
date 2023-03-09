60b08028d217dae44d94068cce264f958f16fc3a
TheTofuKing <72051059+TheTofuKing@users.noreply.github.com>
2023-03-09 17:49:58 -0600

    updated auton

---
+++ src/main/java/frc/robot/commands/intake/slowOuttake.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.intake.IntakeModule;
+import frc.robot.util.Conversions;
+
+public class slowOuttake extends CommandBase {
+
+  IntakeModule intake;
+
+  public slowOuttake(IntakeModule i) {
+    intake = i;
+    addRequirements(i);
+  }
+
+  @Override
+  public void initialize() {
+    intake.setIntakePower(-0.25);
+    new WaitCommand(1);
+    intake.setIntakePower(0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+  
+}
