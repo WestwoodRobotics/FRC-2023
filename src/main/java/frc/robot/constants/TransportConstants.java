39c933e63754f1e587f93d3780e09bb54fbc729b
Razboy20 <razboy20@gmail.com>
2023-03-08 19:20:02 -0600

    arm positions done

--- src/main/java/frc/robot/constants/TransportConstants.java
+++ src/main/java/frc/robot/constants/TransportConstants.java
@@ -11,15 +11,15 @@ public final class TransportConstants {
 
   public static final double kTransportMotorGearRatio = 5.0; //TODO: Verify if this is correct
 
-  public static final double HIGH_SHOULDER_TICKS = 247000;
-  public static final double HIGH_ELBOW_TICKS = 160000;
-  public static final double MID_SHOULDER_TICKS = 272000;
-  public static final double MID_ELBOW_TICKS = 186000;
+  public static final double HIGH_SHOULDER_TICKS = 259520;
+  public static final double HIGH_ELBOW_TICKS = 199368;
+  public static final double MID_SHOULDER_TICKS = 68150;
+  public static final double MID_ELBOW_TICKS = 153506;
   public static final double START_SHOULDER_TICKS = 0;
   public static final double START_ELBOW_TICKS = 0;
-  public static final double GROUND_SHOULDER_TICKS = 0; //wrong
-  public static final double GROUND_ELBOW_TICKS = 125000; //wrong
-  public static final double VERTICAL_SHOULDER_TICKS = 170000;
+  public static final double GROUND_SHOULDER_TICKS = 8429;  
+  public static final double GROUND_ELBOW_TICKS = 111946;
+  public static final double VERTICAL_SHOULDER_TICKS = 173325;
   public static final double VERTICAL_ELBOW_TICKS = 0;
   public static final double SHELF_SHOULDER_TICKS = 66000;
   public static final double SHELF_ELBOW_TICKS = 193000;
