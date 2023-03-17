16e597b89ef5ce4978c60d9268c5645dcc0f9e62
tangerine tnt <afjlaskdfjalsdkfjaskdlfjasd@asd.com>
2023-03-16 20:10:33 -0500

    intake easy w

--- src/main/java/frc/robot/subsystems/intake/IntakeModule.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeModule.java
@@ -2,21 +2,23 @@ package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.PortConstants;
 
 public class IntakeModule extends SubsystemBase {
   // Motor for the intake mechanism
-  private final WPI_TalonFX intakeMotor;
+  private final CANSparkMax intakeMotor;
 
   // Constructor for initializing the intake module
   public IntakeModule() {
-    intakeMotor = new WPI_TalonFX(PortConstants.intakeRollerPort);
+    intakeMotor = new CANSparkMax(PortConstants.intakeRollerPort, MotorType.kBrushless);
     intakeMotor.setInverted(true);
   }
 
   public void setIntakePower(double power) {
-    intakeMotor.set(ControlMode.PercentOutput, power);
+    intakeMotor.set(power);
   }
 }
